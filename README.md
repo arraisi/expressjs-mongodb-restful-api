c4cdf8582f6a73358a194b0207e1e246e2e86b79
Abdul R. Arraisi <arraisi@Abduls-MacBook-Pro.local>
2021-11-13 07:00:53 +0700

    create product service, mongodb

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # expressjs-mongodb-restful-api
+
+# Setup MongoDB
+
+	```
+	docker run  --rm \
+		--name mongo-test \
+		-v /Users/arraisi/System/database/mongo-test/:/etc/mongo \
+		-p 27017:27017 \
+		mongo:4
+	```
+
+	
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
 // import express
 import express from "express";
 // import controllers
-import { getProducts, 
-    getProductById, 
-    saveProduct, 
-    updateProduct,
-    deleteProduct } from '../controllers/productControllers.js';
- 
-    // express router
+import {
+	getProducts,
+	getProductById,
+	findProductById,
+	saveProduct,
+	updateProduct,
+	deleteProduct
+} from '../controllers/productControllers.js';
+
+// express router
 const router = express.Router();
- 
+
 // Route get All Products
 router.get('/', getProducts);
 // Route get single Product
-router.get('/:id', getProductById);
+router.get('/:id', findProductById);
 // Route CREATE Product
 router.post('/', saveProduct);
 // Route UPDATE Product
 router.patch('/:id', updateProduct);
 // Route DELETE Product
 router.delete('/:id', deleteProduct);
- 
+
 // export router
 export default router;
